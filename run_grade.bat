d5cd431c474c20da6065286f84a2bfbe7bb1f963
Ningyu Wang <wny001@gmail.com>
2021-03-30 15:16:04 -0500

    Delete run_grade.bat

--- run_grade.bat
+++
@@ -1,7 +0,0 @@
-REM This file runs the test.py file in Windows command window.
-REM You may also copy paste the commands into the Windows command window to run them.
-REM Python needs to be installed in your computer.
-REM Depending on your program, additional packages may be needed.
-REM The -v option ensures the correct answers are also shown on the screen.
-Python grade.py -v
-pause
